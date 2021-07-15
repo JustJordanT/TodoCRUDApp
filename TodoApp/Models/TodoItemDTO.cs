2b08efbbbd1495d2c114db3a6574d030a19c2954
JustJordanT <jordantay9014@gmail.com>
2021-07-15 07:03:10 -0500

    Add project files.

---
+++ TodoApp/Models/TodoItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class TodoItemDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
